defeca2c2c4c4652ac65c0ec65cd6d4c1e650bca
Noravorn <144671016+Noravorn@users.noreply.github.com>
2023-09-12 18:52:30 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,22 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
     x, y, z = s
-    return []
+    # Define the capacities of the bottles
+    capacities = (8, 5, 3)
+    # Define the possible actions
+    actions = []
+    for i in range(3):
+        for j in range(3):
+            if i != j:
+                # Pour water from bottle i to bottle j
+                amount = min(x[i], capacities[j] - x[j])
+                state = list(x)
+                state[i] -= amount
+                state[j] += amount
+                actions.append((tuple(state), amount))
+    return actions
